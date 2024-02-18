91c3f56781540eff76ab77cdd4f25e3a379c77b1
Ahmed Mahrous <ahmed.mahrous@gomini-app.com>
2024-02-18 18:57:33 +0200

    Update SceneDelegate

--- MarvelApp/SceneDelegate.swift
+++ MarvelApp/SceneDelegate.swift
@@ -2,7 +2,7 @@
 //  SceneDelegate.swift
 //  MarvelApp
 //
-//  Created by Ahmed Mahrous on 18/02/2024.
+//  Created by Ahmed Mahrous on 17/02/2024.
 //
 
 import UIKit
@@ -16,7 +16,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        //MARK: - App EntryPoint
+        //Change module style from storyboard to nib
+        
+        let nav = NavigationViewController(rootViewController: SplashViewController())
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = nav
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
